fc7c8ef137c121bd9dff0f4423c08b585f24e1d1
Ng <ngb@curtisinst.com>
2020-02-29 01:53:59 -0800

    first working copy

---
+++ AutoLogger.py
@@ -0,0 +1,73 @@
+# resumes gracefully from sleep
+
+from PCANBasic import *
+import time
+import os
+import msvcrt
+import datetime
+
+os.add_dll_directory(os.getcwd())
+
+# constants
+channel = PCAN_USBBUS1
+baud = PCAN_BAUD_500K
+heartbeat_timeout = 15  # seconds
+tic_sleep_period = .001
+
+pcan = PCANBasic()
+
+status = pcan.Initialize(channel, baud)
+if (status != PCAN_ERROR_OK):
+    print("Error initializing CAN dongle")
+    exit()
+else:
+    print("Successfully initialized CAN dongle. Waiting for heartbeats...")
+
+pcan.Reset(channel)
+pcan.SetValue(channel, PCAN_TRACE_CONFIGURE, TRACE_FILE_SEGMENTED | TRACE_FILE_DATE | TRACE_FILE_TIME)
+pcan.SetValue(channel, PCAN_TRACE_SIZE, 100)
+
+
+logging_active = False
+def start_logging():
+    pcan.SetValue(channel, PCAN_TRACE_STATUS, PCAN_PARAMETER_ON)
+    global logging_active
+    logging_active = True
+
+def stop_logging():
+    pcan.SetValue(channel, PCAN_TRACE_STATUS, PCAN_PARAMETER_OFF)
+    global logging_active
+    logging_active = False
+
+
+heartbeat_timer = heartbeat_timeout
+
+while (True):
+
+    time.sleep(tic_sleep_period)
+    read_result = pcan.Read(channel)
+    msg = read_result[1]
+
+    heartbeat_found_flag = False
+
+    # if (msvcrt.kbhit()):
+    #     msvcrt.getch()
+    #     heartbeat_found_flag = True
+
+    if(msg.MSGTYPE == PCAN_MESSAGE_STANDARD.value):
+        # got a good message
+        if(msg.ID == 0x729):
+            # got a heartbeat
+            heartbeat_timer = heartbeat_timeout  # reset heartbeat timer
+            heartbeat_found_flag = True
+
+    if (heartbeat_found_flag):
+        if (not logging_active):
+            print("Detected Heartbeat. Starting Log...\t\t\t\tCurrent Time: " + str(datetime.datetime.now()))
+            start_logging()
+    elif (logging_active):
+        heartbeat_timer -= tic_sleep_period
+
+    if (heartbeat_timer <= 0 and logging_active):
+        print("Lost Heatbeat. Stopping Log")
+        stop_logging()
